932bcf1aca7293074d5bb65c8f332e93536513c8
Teja <www.vendrateja2006@gmail.com>
2026-07-26 23:45:42 +0530

    redis is oning

--- redis/redisServer.js
+++ redis/redisServer.js
@@ -3,4 +3,9 @@ import Redis from 'ioredis'
 
 const client = new Redis("redis://localhost:6379");
 
+const res = await client.set("user:4","pavan","NX");
+console.log('====================================');
+console.log(res);
+console.log('====================================');
+
 export default client;
